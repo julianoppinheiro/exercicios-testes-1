663722ac0c8dddef4dc9db43f23ad28900295e7a
DESKTOP-86EIRIU\Scond <juliano_pimentel@live.com>
2023-05-18 22:46:20 -0300

    [M3S01] Ex 3 - Crie os testes unitários para os métodos atualizarCliente

--- exercicio-testes/src/test/java/com/example/exerciciotestes/M3S01_exercicio03.java
+++ exercicio-testes/src/test/java/com/example/exerciciotestes/M3S01_exercicio03.java
@@ -1,38 +1,38 @@
 package com.example.exerciciotestes;
 
 import com.example.exerciciotestes.controller.request.ClienteRequest;
-import com.example.exerciciotestes.model.Cliente;
 import com.example.exerciciotestes.repository.ClienteRepository;
 import com.example.exerciciotestes.service.ClienteService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
-class M3S01_exercicio02 {
+class M3S01_exercicio03 {
 
-    //Crie os testes unitários para os métodos salvarCliente já existente na classe ClienteService
     @Test
-    @DisplayName("Deve salvar um cliente")
-    public void salvarCliente() {
+    @DisplayName("Deve atualizar um cliente no banco de dados")
+
+// Crie os testes unitários para os métodos atualizarCliente já existente na classe ClienteService
+    public void atualizarCliente() {
         // Cria um objeto ClienteService e o objeto ClienteResquest mockado
         ClienteService clienteService = new ClienteService(mock(ClienteRepository.class));
         ClienteRequest clienteRequest = mock(ClienteRequest.class);
 
-        //atribui valores ao objeto mock
-        when(clienteRequest.getNomeCliente()).thenReturn("aa");
+        //atribui valores ao objeto mock para atualizar o cliente
+        when(clienteRequest.getNomeCliente()).thenReturn("atualizado");
         when(clienteRequest.getSaldoCliente()).thenReturn(10.0);
 
         //salvar cliente
-        clienteService.salvarCliente(clienteRequest);
+        clienteService.atualizarCliente(1L, clienteRequest);
 
         // Verifique se o comportamento esperado ocorreu usando o Mockito
-        Assertions.assertEquals("aa", clienteRequest.getNomeCliente());
+        Assertions.assertEquals("atualizado", clienteRequest.getNomeCliente());
+        Assertions.assertEquals(10.0, clienteRequest.getSaldoCliente());
 
     }
 }
